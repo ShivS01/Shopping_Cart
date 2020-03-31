a543b62e93a55d00abde29c3df8d8536e01cdb0b
harshinvader <emails.harsha@gmail.com>
2020-03-31 16:47:11 +0530

    more icons

--- modules/tktoolbar.py
+++ modules/tktoolbar.py
@@ -17,27 +17,27 @@ def _init_toolbar(tbmaster):
     tbmaster.tb.pack(side=TOP,fill=X)#####
     imgdir="images/16x16/"
     tbmaster.btn_add=Button(tbmaster.tb,command=tbmaster.btn_add_click)
-    tbmaster.imgadd=PhotoImage(file=imgdir+"add.gif")
+    tbmaster.imgadd=PhotoImage(file=imgdir+"add.png")
     tbmaster.btn_add['image']=tbmaster.imgadd
     tbmaster.btn_add.pack(side=LEFT,padx=4,pady=4)
     CreateToolTip(tbmaster.btn_add, text='Add a Product')
     
     tbmaster.btn_edit=Button(tbmaster.tb,command=tbmaster.btn_edit_click)
-    tbmaster.imgedit=PhotoImage(file=imgdir+"edit2.gif")
+    tbmaster.imgedit=PhotoImage(file=imgdir+"edit2.png")
     tbmaster.btn_edit['image']=tbmaster.imgedit
     tbmaster.btn_edit.pack(side=LEFT,padx=4,pady=4)
     CreateToolTip(tbmaster.btn_edit, text='Edit a Product')
 
     
     tbmaster.btn_delete=Button(tbmaster.tb,command=tbmaster.btn_del_click)
-    tbmaster.imgdel=PhotoImage(file=imgdir+"delete.gif")
+    tbmaster.imgdel=PhotoImage(file=imgdir+"delete.png")
     tbmaster.btn_delete['image']=tbmaster.imgdel
     tbmaster.btn_delete.pack(side=LEFT,padx=4,pady=4)
     CreateToolTip(tbmaster.btn_delete, text='Delete a Product')
 
     
     tbmaster.btn_find=Button(tbmaster.tb,command=tbmaster.tb_btnfind_click)
-    tbmaster.imgfind=PhotoImage(file=imgdir+"find.gif")
+    tbmaster.imgfind=PhotoImage(file=imgdir+"find.png")
     tbmaster.btn_find['image']=tbmaster.imgfind
     tbmaster.btn_find.pack(side=LEFT,padx=4,pady=4)
     CreateToolTip(tbmaster.btn_find, text='Find a Product')
